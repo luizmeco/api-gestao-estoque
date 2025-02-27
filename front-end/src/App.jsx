3cbe93c3bd6ee83e0ff6ff7c969e644dd83451b1
Luiz Américo Valandro <meco.valandro@gmail.com>
2025-02-27 17:13:41 -0300

    ajustes das rotas PUT e DELETE, e melhora na UX do app

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     return (
         <div className='bg-dark pb-3'>
             <NavBar />
-            <div className='col-md-10 mx-auto text-center bg-primary bg-opacity-50 mt-4 rounded-4 fs-2 p-3 '>
+            <div className='col-md-10 mx-auto text-center bg-primary bg-opacity-50 mt-4 rounded-4 fs-2 p-3 shadow-lg'>
                 <Outlet />
             </div>
         </div>
--- server.js
+++ server.js
@@ -1,32 +1,59 @@
 import "dotenv/config";
 import express from "express";
-import { PrismaClient } from '@prisma/client'
-import cors from "cors"
+import { PrismaClient } from "@prisma/client";
+import cors from "cors";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 var app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get("/consulta",async (req, res) => {
-    var listaMovimentacoes = await prisma.movimentacoes.findMany()
+app.get("/consulta", async (req, res) => {
+  var listaMovimentacoes = await prisma.movimentacoes.findMany();
   res.json(listaMovimentacoes);
 });
 
 app.post("/cadastrar", async (req, res) => {
-    res.send("Olá Mundo!");
-    console.log(req.body);
-    await prisma.movimentacoes.create({
-        data: {
-            data: req.body.data,
-            situacao: req.body.situacao,
-            produto: req.body.produto,
-            peso: req.body.peso,
-            valor: req.body.valor
-        }
-    })
-})
+  res.send("Olá Mundo!");
+  console.log(req.body);
+  await prisma.movimentacoes.create({
+    data: {
+      data: req.body.data,
+      situacao: req.body.situacao,
+      produto: req.body.produto,
+      peso: req.body.peso,
+      valor: req.body.valor,
+    },
+  });
+});
+
+app.put("/atualizar/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  await prisma.movimentacoes.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      data: req.body.data,
+      situacao: req.body.situacao,
+      produto: req.body.produto,
+      peso: req.body.peso,
+      valor: req.body.valor,
+    },
+  });
+});
+
+app.delete("/deletar/:id", async (req, res) => {
+  const userId = req.params.id;
+  await prisma.movimentacoes.delete({
+    where: {
+      id: userId,
+    },
+  });
+});
+
 app.listen(process.env.PORT);
 
 console.log("Servidor escutando... http://localhost:8080/");
